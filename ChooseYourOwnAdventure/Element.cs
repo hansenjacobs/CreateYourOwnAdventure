ceb2130d2f6bd2e5539a5700d47134940e711d2c
Jacob Hansen <hansenjacobs@gmail.com>
2018-04-26 10:45:33 -0500

    created Element class member variables/methods

---
+++ ChooseYourOwnAdventure/Element.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourOwnAdventure
+{
+    class Element
+    {
+        string story;
+        string choice;
+        int optionAElement;
+        int optionBElement;
+
+        public Element(string story, string choice, int optionAElement, int optionBElement)
+        {
+            this.story = story;
+            this.choice = choice;
+            this.optionAElement = optionAElement;
+            this.optionBElement = optionBElement;
+        }
+
+        public void DisplayElement()
+        {
+            Console.WriteLine(story);
+            if(choice != "")
+            {
+                Console.WriteLine(choice);
+            }
+        }
+
+        public int GetNextElement()
+        {
+            string input = Console.ReadKey().KeyChar.ToString().ToLower();
+
+            switch (input)
+            {
+                case "a":
+                    return optionAElement;
+
+                case "b":
+                    return optionBElement;
+
+                default:
+                    Console.WriteLine("Invalid input! '" + input + "' is not a valid option.  Please try again.\n");
+                    DisplayElement();
+                    return 0;
+            }
+        }
+    }
+}
